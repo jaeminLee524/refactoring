ccfa80c3e7c8536a379fb59b2322e51b49e82092
이재민 <rkwoals524@hanmail.net>
2025-01-12 20:01:41 +0900

    refactor 6. remove unreachable branches

--- src/main/java/com/study/repeating_switch/NorwegianBlueParrot.java
+++ src/main/java/com/study/repeating_switch/NorwegianBlueParrot.java
@@ -1,7 +1,5 @@
 package com.study.repeating_switch;
 
-import static com.study.repeating_switch.ParrotTypeEnum.NORWEGIAN_BLUE;
-
 public class NorwegianBlueParrot extends Parrot {
 
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
@@ -10,10 +8,6 @@ public class NorwegianBlueParrot extends Parrot {
 
     @Override
     public double getSpeed() {
-        return switch (NORWEGIAN_BLUE) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
 }
