95151cb84cf83e643d3aa2d4bfe2952e952f71cf
이재민 <rkwoals524@hanmail.net>
2025-01-12 20:03:58 +0900

    refactor 9. Replace dynamic type-based switch with fixed constant for EuropeanParrot

--- src/main/java/com/study/repeating_switch/EuropeanParrot.java
+++ src/main/java/com/study/repeating_switch/EuropeanParrot.java
@@ -1,5 +1,7 @@
 package com.study.repeating_switch;
 
+import static com.study.repeating_switch.ParrotTypeEnum.EUROPEAN;
+
 public class EuropeanParrot extends Parrot {
 
     public EuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
@@ -8,7 +10,7 @@ public class EuropeanParrot extends Parrot {
 
     @Override
     public double getSpeed() {
-        return switch (type) {
+        return switch (EUROPEAN) {
             case EUROPEAN -> getBaseSpeed();
             case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
             case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
